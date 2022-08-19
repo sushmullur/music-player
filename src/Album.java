026d475450a836276912cb1195560e237a49f1f6
sushpicious <2011658@apps.nsd.org>
2022-08-18 20:18:11 -0700

    Initial commit

---
+++ src/Album.java
@@ -0,0 +1,11 @@
+public class Album {
+
+    private String name;
+    private Artist artist;
+    private int length;
+    private int duration;
+    // Private Artist collection of features
+    private int yearOfRelease;
+    // Private Queue of songs
+
+}
---
+++ src/Artist.java
@@ -0,0 +1,8 @@
+public class Artist {
+
+    private String realName;
+    private String stageName;
+    // Private collection of albums
+    // Private Song collection of discography
+    private int streams;
+}
